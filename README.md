e3bc65f669b61ccee52283eedda29178482ddc6f
Nate Lee <mengyong.lee@thoughtworks.com>
2025-06-12 23:59:03 +0800

    first working demo version
    cleanup
    
    remove warning
    
    remove tw relevant keywords

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Learning MCP
+# jigsaw-search-mcp
 
+Demo on using MCP to query jigsaw data.
+
+To start, 
+
+1. Run `pip install -r requirements.txt`
+2. Create .env (based on .env.example), and fill in your api keys
+3. Run `python -m src.mcp_chatbot `
+
+Resources:
+- Thoughtworks Neo
+- Deeplearning.ai course: https://learn.deeplearning.ai/courses/mcp-build-rich-context-ai-apps-with-anthropic/lesson/fkbhh/introduction
---
+++ src/jigsaw_server.py
@@ -0,0 +1,73 @@
+import os
+from typing import List
+import requests
+from dotenv import load_dotenv
+import urllib3
+
+from mcp.server.fastmcp import FastMCP
+
+load_dotenv()
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+url = os.getenv("OKTA_OAUTH_URL")
+auth = (os.getenv("NEO_USERNAME"), os.getenv("NEO_PASSWORD"))
+headers = {"content-type": "application/x-www-form-urlencoded"}
+data = {"grant_type": "client_credentials",
+        "scope": "ApiCommonReadAccess"
+}
+
+response = requests.post(url, auth=auth, headers=headers, data=data, verify=False)
+access_token = response.json().get("access_token")
+
+# Initialize FastMCP server
+mcp = FastMCP("jigsaw_server")
+print("Starting Jigsaw server...")
+
+
+@mcp.tool()
+def search_role(role: str, working_office: str = "Singapore") -> List[str]:
+    """
+    Search for people on Jigsaw based on a role, grade, and working office.
+
+    Role: The specific role to search for (e.g., "Developer", "Machine Learning Engineer", "Data Scientist", "Experience Designer", "Product Manager", "Business Analyst").
+    Working Office: The office location to filter results (default is "Singapore").
+    """
+
+    url = os.getenv("JIGSAW_PEOPLE_URL")
+    params = {
+        "working_office": working_office,
+        "role": f"{role}"
+    }
+    headers = {
+        "authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(url, headers=headers, params=params, verify=False)
+
+    if response.status_code == 200:
+        return response.json()  # full list of person dicts
+    else:
+        print(f"Error fetching data from Jigsaw: {response.status_code}")
+        return []
+
+
+@mcp.prompt()
+def generate_people_search_prompt(role: str, working_office: str = "Singapore") -> str:
+    """
+    Generate a natural language prompt to search for people based on role, grade, and working office.
+
+    role: The specific role to search for (e.g., "Developer", "Machine Learning Engineer", "Data Scientist", "Experience Designer", "Product Manager", "Business Analyst").
+    working_office: The office location to filter results (default is "Singapore") (e.g. New York, London, Singapore, ).
+
+    Returns: A prompt string to search for matching people.
+    """
+    return (
+        f"Find all people with the role '{role}', "
+        f"and working in the '{working_office}' office."
+    )
+
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='stdio')
